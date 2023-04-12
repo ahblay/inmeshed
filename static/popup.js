f95d352384983ed17b77f88cc02f1aae718c55bc
Abel Romer <abel.romer@questu.ca>
2023-04-12 15:21:15 -0700

    Adds how to play modal, icons and new navbar, and restyles header and footer.

--- static/popup.js
+++ static/popup.js
@@ -3,15 +3,19 @@ $("#progress-bar").click(function() {
     $("#score-modal").css("display", "block");
 });
 
+$("#info-icon").click(function() {
+    $("#info-modal").css("display", "block");
+});
+
 // When the user clicks on <span> (x), close the modal
 $(".close").click(function() {
-    $("#score-modal").css("display", "none");
+    $(".modal").css("display", "none");
 });
 
 // When the user clicks anywhere outside of the modal, close it
 $(document).click(function(event) {
-    if (event.target == $("#score-modal")[0]) {
-        $("#score-modal").css("display", "none");
+    if (event.target == $(".modal:visible")[0]) {
+        $(".modal").css("display", "none");
         $("#guess-input").focus();
     }
 });
